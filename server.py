0cd52614d7c75b2c5de2416c026c418915a3428b
wjoelmendoza <wjoelmendoza@gmail.com>
2020-04-17 08:33:44 -0600

    agregando factorial

--- server.py
+++ server.py
@@ -6,11 +6,25 @@ app = Flask(__name__)
 def hello():
     return "Hola mundo"
 
+
 @app.route("/saludo")
 @app.route("/saludo/<nombre>")
 def saludo(nombre=None):
     return render_template('index.html', nombre=nombre)
 
+
+@app.route("/factorial/<valor>")
+def fact(valor):
+    iv = int(valor)
+    resultado = factorial(iv)
+    return render_template("factorial.html", valor=valor, resultado=resultado)
+
+
+def factorial(num):
+    if num <= 0:
+        return 1
+    else:
+        return num * factorial(num - 1)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080, debug=True)
