981a3648258a4dcb721765bd1a465d3b62bed77b
wjoelmendoza <wjoelmendoza@gmail.com>
2020-04-17 08:22:30 -0600

    agregando plantillas

--- server.py
+++ server.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/hello")
 def hello():
     return "Hola mundo"
+
+@app.route("/saludo")
+@app.route("/saludo/<nombre>")
+def saludo(nombre=None):
+    return render_template('index.html', nombre=nombre)
 
 
 if __name__ == "__main__":
